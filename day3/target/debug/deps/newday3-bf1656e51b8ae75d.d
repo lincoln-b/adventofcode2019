f9839c7b727ffd9abdea6da69a793b5b6f11106d
Lincoln Bergeson <lbergeson@qualtrics.com>
2019-12-10 12:16:12 -0700

    removed some files that should have been ignored

--- day3/target/debug/deps/newday3-bf1656e51b8ae75d.d
+++
@@ -1,5 +0,0 @@
-/Users/i510882/Personal/adventofcode2019/day3/target/debug/deps/newday3-bf1656e51b8ae75d: src/main.rs
-
-/Users/i510882/Personal/adventofcode2019/day3/target/debug/deps/newday3-bf1656e51b8ae75d.d: src/main.rs
-
-src/main.rs:
--- day3/target/debug/newday3.d
+++
@@ -1,1 +0,0 @@
-/Users/i510882/Personal/adventofcode2019/day3/target/debug/newday3: /Users/i510882/Personal/adventofcode2019/day3/src/main.rs

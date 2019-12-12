9d7d37c4c048a74a40bebb6614ee9a463ea1f113
Lincoln Bergeson <lbergeson@qualtrics.com>
2019-12-12 16:43:33 -0700

    day 7

---
+++ day7/src/main.rs
@@ -0,0 +1,81 @@
+mod input;
+mod program;
+
+use program::IntCode;
+
+fn main() {
+
+    // phase 1
+
+    let permutations = calculate_permutations(vec![0, 1, 2, 3, 4]);
+    let mut max = 0;
+    for p in &permutations {
+        let val = run_program_with_settings(p);
+        if val > max {
+            max = val;
+        }
+    }
+    println!("{:?}", max);
+
+    // phase 2
+
+    let permutations = calculate_permutations(vec![5, 6, 7, 8, 9]);
+    let mut max = 0;
+    for p in permutations {
+        let val = loop_program_with_settings(&p);
+        if val > max {
+            max = val;
+        }
+    }
+    println!("{:?}", max);
+}
+
+fn loop_program_with_settings(settings: &Vec<i32>) -> i32 {
+    let mut last_out = 0;
+    let mut computers = vec![
+        IntCode::new(&input::ORIGINAL),
+        IntCode::new(&input::ORIGINAL),
+        IntCode::new(&input::ORIGINAL),
+        IntCode::new(&input::ORIGINAL),
+        IntCode::new(&input::ORIGINAL),
+    ];
+    for (i, phase) in settings.iter().enumerate() {
+        last_out = computers[i].run(&[*phase,last_out]).unwrap();
+    }
+    let mut i = 0;
+    loop {
+        last_out = match computers[i].run(&[last_out]) {
+            Some(v) => v,
+            None => {
+                return last_out;
+            }
+        };
+        i = if i == 4 { 0 } else { i + 1 };
+    }
+}
+
+fn run_program_with_settings(settings: &Vec<i32>) -> i32 {
+    let mut last_out = 0;
+    for phase in settings {
+        last_out = IntCode::new(&input::ORIGINAL).run(&[*phase,last_out]).unwrap();
+    }
+    last_out
+}
+
+fn calculate_permutations(vec: Vec<i32>) -> Vec<Vec<i32>> {
+    if vec.len() <= 1 {
+        return vec![vec];
+    }
+
+    let mut out: Vec<Vec<i32>> = Vec::new();
+    for (i, x) in vec.iter().enumerate() {
+        let mut vec_cpy = vec.clone();
+        vec_cpy.remove(i);
+        for v in calculate_permutations(vec_cpy) {
+            let mut vec_cpy = vec![*x];
+            vec_cpy.extend(v);
+            out.push(vec_cpy);
+        }
+    }
+    out
+}
---
+++ day7/src/program.rs
@@ -0,0 +1,133 @@
+pub struct IntCode {
+    memory: Vec<i32>,
+    counter: usize,
+}
+
+#[derive(Debug)]
+enum Mode {
+    Position,
+    Immediate,
+}
+
+#[derive(Debug)]
+enum Instruction {
+    Add(Mode, Mode),
+    Multiply(Mode, Mode),
+    Input,
+    Output(Mode),
+    JumpIfTrue(Mode, Mode),
+    JumpIfFalse(Mode, Mode),
+    LessThan(Mode, Mode),
+    Equals(Mode, Mode),
+    Break,
+}
+
+impl Instruction {
+    fn parse(opcode: i32) -> Instruction {
+        let mut opcode = opcode;
+        let mut first_mode = Mode::Position;
+        let mut second_mode = Mode::Position;
+        if opcode >= 1_000 {
+            second_mode = Mode::Immediate;
+            opcode -= 1_000;
+        }
+        if opcode >= 100 {
+            first_mode = Mode::Immediate;
+            opcode -= 100;
+        }
+        match opcode {
+            1 => Instruction::Add(first_mode, second_mode),
+            2 => Instruction::Multiply(first_mode, second_mode),
+            3 => Instruction::Input,
+            4 => Instruction::Output(first_mode),
+            5 => Instruction::JumpIfTrue(first_mode, second_mode),
+            6 => Instruction::JumpIfFalse(first_mode, second_mode),
+            7 => Instruction::LessThan(first_mode, second_mode),
+            8 => Instruction::Equals(first_mode, second_mode),
+            99 => Instruction::Break,
+            _ => panic!("opcode {} not recognized", opcode),
+        }
+    }
+}
+
+impl IntCode {
+
+    pub fn new(mem: &[i32]) -> IntCode {
+        IntCode {
+            memory: mem.to_vec(),
+            counter: 0,
+        }
+    }
+
+    fn get(&self, mode: Mode, index: usize) -> i32 {
+        match mode {
+            Mode::Position => self.memory[self.memory[index] as usize],
+            Mode::Immediate => self.memory[index],
+        }
+    }
+
+    pub fn run(&mut self, input: &[i32]) -> Option<i32> {
+        let mut input = input.iter();
+        while self.counter < self.memory.len() {
+            match Instruction::parse(self.memory[self.counter]) {
+                Instruction::Add(a, b) => {
+                    let dest = self.memory[self.counter + 3] as usize;
+                    self.memory[dest] = self.get(a, self.counter + 1) + self.get(b, self.counter + 2);
+                    self.counter += 4;
+                }
+                Instruction::Multiply(a, b) => {
+                    let dest = self.memory[self.counter + 3] as usize;
+                    self.memory[dest] = self.get(a, self.counter + 1) * self.get(b, self.counter + 2);
+                    self.counter += 4;
+                }
+                Instruction::Input => {
+                    let i1 = self.memory[self.counter + 1] as usize;
+                    self.memory[i1] = *input.next().unwrap();
+                    self.counter += 2
+                }
+                Instruction::Output(a) => {
+                    let val = Some(self.get(a, self.counter + 1));
+                    self.counter += 2;
+                    return val;
+                }
+                Instruction::JumpIfTrue(a, b) => {
+                    if self.get(a, self.counter + 1) != 0 {
+                        self.counter = self.get(b, self.counter + 2) as usize;
+                    } else {
+                        self.counter += 3;
+                    }
+                }
+                Instruction::JumpIfFalse(a, b) => {
+                    if self.get(a, self.counter + 1) == 0 {
+                        self.counter = self.get(b, self.counter + 2) as usize;
+                    } else {
+                        self.counter += 3;
+                    }
+                }
+                Instruction::LessThan(a, b) => {
+                    let i3 = self.memory[self.counter + 3] as usize;
+                    if self.get(a, self.counter + 1) < self.get(b, self.counter + 2) {
+                        self.memory[i3] = 1;
+                    } else {
+                        self.memory[i3] = 0;
+                    }
+                    self.counter += 4;
+                }
+                Instruction::Equals(a, b) => {
+                    let i3 = self.memory[self.counter + 3] as usize;
+                    if self.get(a, self.counter + 1) == self.get(b, self.counter + 2) {
+                        self.memory[i3] = 1;
+                    } else {
+                        self.memory[i3] = 0;
+                    }
+                    self.counter += 4;
+                }
+                Instruction::Break => {
+                    break;
+                }
+            };
+        }
+        None
+    }
+}
+

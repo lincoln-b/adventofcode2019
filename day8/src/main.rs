70ac166df6cbbd6a8deac5d177fd953510cbc00f
Lincoln Bergeson <lbergeson@qualtrics.com>
2019-12-12 17:13:14 -0700

    day 8

---
+++ day8/src/main.rs
@@ -0,0 +1,73 @@
+use std::fs;
+
+fn main() {
+    
+    let layers = load_image("data.txt");
+
+    // part 1
+
+    let mut min_index = 0;
+    let mut min_value = 10000;
+    for (i, layer) in layers.iter().enumerate() {
+        let val = layer.iter().filter(|x| { **x == '0' }).count();
+        if val < min_value {
+            min_value = val;
+            min_index = i;
+        }
+    }
+
+    let num1 = layers[min_index].iter().filter(|x| { **x == '1' }).count();
+    let num2 = layers[min_index].iter().filter(|x| { **x == '2' }).count();
+    println!("{}", num1 * num2);
+
+    // part 2
+    
+    let layer = combine_layers(&layers);
+    print_layer(&layer);
+
+}
+
+fn combine_layers(layers: &Vec<Vec<char>>) -> Vec<char> {
+    let mut combined_layer = vec!['2'; 150];
+    for layer in layers {
+        for i in 0..150 {
+            if combined_layer[i] == '2' {
+                combined_layer[i] = layer[i];
+            }
+        }
+    }
+    combined_layer
+}
+
+fn print_layer(layer: &Vec<char>) {
+    for i in 0..6 {
+        for j in 0..25 {
+            if layer[i * 25 + j] == '0' {
+                print!(" ");
+            } else {
+                print!("{}", layer[i * 25 + j]);
+            }
+        }
+        println!();
+    }
+}
+
+fn load_image(filename: &str) -> Vec<Vec<char>> {
+    let data = fs::read_to_string(filename).unwrap();
+
+    let mut layers = Vec::new();
+    let mut layer = Vec::new();
+    let mut i = 0;
+
+    for c in data.chars() {
+        layer.push(c);
+        i = if i == 149 { 
+            layers.push(layer);
+            layer = Vec::new();
+            0
+        } else {
+            i + 1
+        }
+    }
+    layers
+}
